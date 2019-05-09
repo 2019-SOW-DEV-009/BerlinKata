3c2fa9c3a9a1a0d49fc8239d13fde1af89926e9f
2019_SOW_DEV_009 <2019.SOW.DEV.009@gmail.com>
2019-05-09 12:43:37 +0200

    extracted the minute validation into a method

--- app/src/main/java/com/kata/berlin/digitaltime/DigitalMinute.java
+++ app/src/main/java/com/kata/berlin/digitaltime/DigitalMinute.java
@@ -4,16 +4,20 @@ public class DigitalMinute {
     private final int minute;
 
     public DigitalMinute(int minute) throws InvalidDigitalMinuteException {
+        validate(minute);
+        this.minute = minute;
+    }
+
+    public int minute() {
+        return minute;
+    }
+
+    private void validate(int minute) throws InvalidDigitalMinuteException {
         if (minute < 0) {
             throw new InvalidDigitalMinuteException("Minute cannot be in negative");
         }
         if (minute > 59) {
             throw new InvalidDigitalMinuteException("Minute cannot be more than 59");
         }
-        this.minute = minute;
-    }
-
-    public int minute() {
-        return minute;
     }
 }
