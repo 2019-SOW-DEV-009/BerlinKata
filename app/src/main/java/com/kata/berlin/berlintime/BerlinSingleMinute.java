ef9148b95cf30a417781dab73536522120dd52f5
2019_SOW_DEV_009 <2019.SOW.DEV.009@gmail.com>
2019-05-09 13:34:53 +0200

    removed the switch case with final set of hashmap for single minute

--- app/src/main/java/com/kata/berlin/berlintime/BerlinSingleMinute.java
+++ app/src/main/java/com/kata/berlin/berlintime/BerlinSingleMinute.java
@@ -2,6 +2,8 @@ package com.kata.berlin.berlintime;
 
 import com.kata.berlin.digitaltime.DigitalMinute;
 
+import java.util.HashMap;
+
 public class BerlinSingleMinute {
 
     private static final String ZERO_MINUTE = "OOOO";
@@ -9,6 +11,17 @@ public class BerlinSingleMinute {
     private static final String TWO_MINUTE = "YYOO";
     private static final String THREE_MINUTE = "YYYO";
     private static final String FOUR_MINUTE = "YYYY";
+    private static final HashMap<Integer, String> SINGLE_MINUTE_MAP;
+
+    static {
+        SINGLE_MINUTE_MAP = new HashMap<>();
+        SINGLE_MINUTE_MAP.put(0, ZERO_MINUTE);
+        SINGLE_MINUTE_MAP.put(1, ONE_MINUTE);
+        SINGLE_MINUTE_MAP.put(2, TWO_MINUTE);
+        SINGLE_MINUTE_MAP.put(3, THREE_MINUTE);
+        SINGLE_MINUTE_MAP.put(4, FOUR_MINUTE);
+    }
+
     private final DigitalMinute digitalMinute;
 
     public BerlinSingleMinute(DigitalMinute digitalMinute) {
@@ -17,17 +30,6 @@ public class BerlinSingleMinute {
 
     public String singleMinute() {
         int proportionToFive = digitalMinute.minute() % 5;
-        switch (proportionToFive) {
-            case 1:
-                return ONE_MINUTE;
-            case 2:
-                return TWO_MINUTE;
-            case 3:
-                return THREE_MINUTE;
-            case 4:
-                return FOUR_MINUTE;
-            default:
-                return ZERO_MINUTE;
-        }
+        return SINGLE_MINUTE_MAP.get(proportionToFive);
     }
 }
