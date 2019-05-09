b0ce27f28e1a06ee3ae7c063655066100438f567
2019_SOW_DEV_009 <2019.SOW.DEV.009@gmail.com>
2019-05-09 21:46:26 +0200

    Extracted presenter and view as fields and initiated them in setup

--- app/src/test/java/com/kata/berlin/presenter/BerlinTimePresenterTest.java
+++ app/src/test/java/com/kata/berlin/presenter/BerlinTimePresenterTest.java
@@ -4,6 +4,7 @@ import com.kata.berlin.berlintime.BerlinTime;
 import com.kata.berlin.digitaltime.InvalidDigitalTimeException;
 import com.kata.berlin.view.BerlinTimeView;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -12,12 +13,20 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 public class BerlinTimePresenterTest {
+
+    private String inputText;
+    private BerlinTimeView view;
+    private BerlinTimePresenter presenter;
+
+    @Before
+    public void setUp() throws Exception {
+        inputText = "12:45:58";
+        view = mock(BerlinTimeView.class);
+        presenter = new BerlinTimePresenter(view);
+    }
+
     @Test
     public void shouldCallShowBerlinTimeOnViewWhenConvertToBerlinTimeIsCalled() throws InvalidDigitalTimeException {
-        String inputText = "12:45:58";
-        BerlinTimeView view = mock(BerlinTimeView.class);
-        BerlinTimePresenter presenter = new BerlinTimePresenter(view);
-
         presenter.convertToBerlinTime(inputText);
 
         verify(view, times(1)).ShowBerlinTime(any(BerlinTime.class));
