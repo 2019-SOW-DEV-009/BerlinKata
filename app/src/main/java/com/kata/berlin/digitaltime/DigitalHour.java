74cb3b88b42c406f50f856d405ce45cb92e8aab7
2019_SOW_DEV_009 <2019.SOW.DEV.009@gmail.com>
2019-05-09 14:22:17 +0200

    extracted the validation into separate method

--- app/src/main/java/com/kata/berlin/digitaltime/DigitalHour.java
+++ app/src/main/java/com/kata/berlin/digitaltime/DigitalHour.java
@@ -4,16 +4,20 @@ public class DigitalHour {
     private final int hour;
 
     public DigitalHour(int hour) throws InvalidDigitalHourException {
+        validate(hour);
+        this.hour = hour;
+    }
+
+    public int hour() {
+        return hour;
+    }
+
+    private void validate(int hour) throws InvalidDigitalHourException {
         if (hour < 0) {
             throw new InvalidDigitalHourException("Hours cannot be in negative");
         }
         if (hour > 23) {
             throw new InvalidDigitalHourException("Hour cannot be more than 23");
         }
-        this.hour = hour;
-    }
-
-    public int hour() {
-        return hour;
     }
 }
