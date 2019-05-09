acac42621949ec678abfcbc6150a4f59fe194dc2
2019_SOW_DEV_009 <2019.SOW.DEV.009@gmail.com>
2019-05-09 14:34:25 +0200

    removed throws statement in tests

--- app/src/test/java/com/kata/berlin/berlintime/BerlinSingleHourTest.java
+++ app/src/test/java/com/kata/berlin/berlintime/BerlinSingleHourTest.java
@@ -6,25 +6,31 @@ import com.kata.berlin.digitaltime.InvalidDigitalHourException;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class BerlinSingleHourTest {
     @Test
-    public void shouldReturnSingleHourAsOOOOWhenInitiatedWith0Hour() throws InvalidDigitalHourException {
+    public void shouldReturnSingleHourAsOOOOWhenInitiatedWith0Hour() {
         assertSingleHourFor(0, "OOOO");
     }
 
     @Test
-    public void shouldReturnSingleHourAsROOOWhenInitiatedWith1Hour() throws InvalidDigitalHourException {
+    public void shouldReturnSingleHourAsROOOWhenInitiatedWith1Hour() {
         assertSingleHourFor(1, "ROOO");
     }
 
     @Test
-    public void shouldReturnSingleHourAsRROOWhenHourIs2() throws InvalidDigitalHourException {
+    public void shouldReturnSingleHourAsRROOWhenHourIs2() {
         assertSingleHourFor(2, "RROO");
     }
 
-    private void assertSingleHourFor(int hour, String expectedSingleHour) throws InvalidDigitalHourException {
-        DigitalHour digitalHour = new DigitalHour(hour);
+    private void assertSingleHourFor(int hour, String expectedSingleHour) {
+        DigitalHour digitalHour = null;
+        try {
+            digitalHour = new DigitalHour(hour);
+        } catch (InvalidDigitalHourException e) {
+            fail();
+        }
         BerlinSingleHour berlinSingleHour = new BerlinSingleHour(digitalHour);
 
         String singleHour = berlinSingleHour.singleHour();
