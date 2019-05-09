4a5fdf23ed7aa5b97ec3875de5abbfb3ef0c5af3
2019_SOW_DEV_009 <2019.SOW.DEV.009@gmail.com>
2019-05-09 14:11:35 +0200

    created custom assert for digital hour

--- app/src/test/java/com/kata/berlin/digitaltime/DigitalHourTest.java
+++ app/src/test/java/com/kata/berlin/digitaltime/DigitalHourTest.java
@@ -7,19 +7,19 @@ import static org.junit.Assert.assertEquals;
 public class DigitalHourTest {
     @Test
     public void shouldReturnHourAs0WhenInitiatedWith0() {
-        DigitalHour digitalHour = new DigitalHour(0);
-
-        int hour = digitalHour.hour();
-
-        assertEquals(0, hour);
+        assertDigitalHourFor(0, 0);
     }
 
     @Test
     public void shouldReturnHourAs1WhenInitiatedWith1() {
-        DigitalHour digitalHour = new DigitalHour(1);
+        assertDigitalHourFor(1, 1);
+    }
+
+    private void assertDigitalHourFor(int hour1, int expectedHour) {
+        DigitalHour digitalHour = new DigitalHour(hour1);
 
         int hour = digitalHour.hour();
 
-        assertEquals(1, hour);
+        assertEquals(expectedHour, hour);
     }
 }
