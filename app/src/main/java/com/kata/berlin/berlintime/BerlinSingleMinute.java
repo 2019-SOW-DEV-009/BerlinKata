cc7be3185fc298e40a617c26ac10f6d8ad2149e3
2019_SOW_DEV_009 <2019.SOW.DEV.009@gmail.com>
2019-05-09 11:54:09 +0200

    Extracted OOOO into a constant

--- app/src/main/java/com/kata/berlin/berlintime/BerlinSingleMinute.java
+++ app/src/main/java/com/kata/berlin/berlintime/BerlinSingleMinute.java
@@ -3,10 +3,13 @@ package com.kata.berlin.berlintime;
 import com.kata.berlin.digitaltime.DigitalMinute;
 
 public class BerlinSingleMinute {
+
+    private static final String SINGLE_MINUTE = "OOOO";
+
     public BerlinSingleMinute(DigitalMinute digitalMinute) {
     }
 
     public String singleMinute() {
-        return "OOOO";
+        return SINGLE_MINUTE;
     }
 }
